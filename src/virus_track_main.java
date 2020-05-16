430199b99532ee36f91421f26875bbc8f0550d5e
Grim H <bzelban@outlook.com>
2020-05-16 15:21:14 +0300

    Fenrir
    Factory Done, Tests Done

--- src/virus_track_main.java
+++ src/virus_track_main.java
@@ -6,17 +6,16 @@
 
 */
 
-
 // THE FACTORY
-//In here, Factory is for creating phones and adding to the inventories.
+// In here, 2 different factories creating specific phones.
 
-abstract class cellPhone {
+abstract class CellPhone {
     abstract public String displayBrand();
     abstract public String displayModel();
     abstract public void displayAll();
 }
 
-class ApplePhone extends cellPhone{
+class ApplePhone extends CellPhone{
     protected String brand;
     protected String model;
 
@@ -33,12 +32,26 @@ class ApplePhone extends cellPhone{
     {
         this.brand = brand;
         this.model = model;
-
     }
 
 }
 
-class SamsungPhone extends cellPhone{
+abstract class AppleFactory {
+    public static CellPhone createApple(String brand, String model)
+    {
+       if ("Apple".equalsIgnoreCase(brand))
+       {
+           return new ApplePhone("Apple", "iPhone");
+       }
+       else
+       {
+           System.out.println("Wrong Type");
+       }
+       return null;
+    }
+}
+
+class SamsungPhone extends CellPhone{
     protected String brand;
     protected String model;
 
@@ -59,25 +72,10 @@ class SamsungPhone extends cellPhone{
 
 }
 
-abstract class AppleFactory {
-    public static cellPhone cellPhone(String type, String brand, String model)
+abstract class SamsungFactory{
+    public static CellPhone createSamsung(String brand, String model)
     {
-        if("Apple".equalsIgnoreCase(type))
-        {
-            return new ApplePhone("Apple", "iPhone");
-        }
-        else
-        {
-            System.out.println("Wrong Type");
-        }
-        return null;
-    }
-}
-
- class SamsungFactory{
-    public static cellPhone cellPhone(String type, String brand, String mode)
-    {
-        if("Samsung".equalsIgnoreCase(type))
+        if("Samsung".equalsIgnoreCase(brand))
         {
             return new SamsungPhone("Samsung", "Galaxy");
         }
@@ -91,15 +89,25 @@ abstract class AppleFactory {
 
 ////////////////////////////////////////////////////////////////////////////////////
 
+// The RawAppInc.
+// This part ports Raw Native Virus Track app to the specific phone
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
 
 
 public class virus_track_main {
 
-    public static void virus_track_main(String[] args) {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
 
-        //System.out.println("Aight boys Let's do this.\n Leroooooyy JENKIIINNNNNS");
-        cellPhone SamsungPhone = SamsungFactory("Samsung", "Samsung", "Galaxy");
-        cellPhone ApplePhone = AppleFactory("Apple", "Apple", "iPhone");
+        //Aight boys Let's do this: Leroooooyy JENKIIINNNNNS!
+
+        //Factory example here, Need main for User requests.
+        CellPhone iPhone = AppleFactory.createApple("Apple", "iPhone 6");
+        CellPhone Galaxy = SamsungFactory.createSamsung("Samsung", "Galaxy 5");
+
 
         System.out.println("BestBuy Selling Samsung and Apple");
 
