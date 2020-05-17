812fb0929d9260602d8d7e46fa71be3e208aaeac
Grim H <bzelban@outlook.com>
2020-05-17 23:24:28 +0300

    Fenrir
    Patient Database with funcs

---
+++ src/patient.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+
+
+class patient {
+
+    String NAME;
+    String SURNAME;
+    int AGE;
+    Boolean FEWER;
+    Boolean M_ACHE;
+    Boolean R_NOSE;
+
+    public patient() {
+
+    }
+
+    public patient(String NAME, String SURNAME, int AGE, Boolean FEWER, Boolean m_ACHE, Boolean r_NOSE) {
+        this.NAME = NAME;
+        this.SURNAME = SURNAME;
+        this.AGE = AGE;
+        this.FEWER = FEWER;
+        M_ACHE = m_ACHE;
+        R_NOSE = r_NOSE;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public String getSURNAME() {
+        return SURNAME;
+    }
+
+    public void setSURNAME(String SURNAME) {
+        this.SURNAME = SURNAME;
+    }
+
+    public int getAGE() {
+        return AGE;
+    }
+
+    public void setAGE(int AGE) {
+        this.AGE = AGE;
+    }
+
+    public Boolean getFEWER() {
+        return FEWER;
+    }
+
+    public void setFEWER(Boolean FEWER) {
+        this.FEWER = FEWER;
+    }
+
+    public Boolean getM_ACHE() {
+        return M_ACHE;
+    }
+
+    public void setM_ACHE(Boolean m_ACHE) {
+        M_ACHE = m_ACHE;
+    }
+
+    public Boolean getR_NOSE() {
+        return R_NOSE;
+    }
+
+    public void setR_NOSE(Boolean r_NOSE) {
+        R_NOSE = r_NOSE;
+    }
+
+
+
+    private ArrayList<patient> _createPatientTable()
+    {
+
+        ArrayList<patient> patientList = new ArrayList<>();
+        patientList.add(new patient("TEST", "TEST",  99, false, false, false));
+
+
+        return patientList;
+    }
+
+
+    public String toString(patient x) {
+        String print=x.getNAME() +"  "+  x.getSURNAME() +" Age: "+ x.getAGE() +" Fewer?: "+ x.getFEWER() + " Muscle Ache?: "+ x.getM_ACHE() + " Runny Nose?:" + x.getR_NOSE();
+        return print;
+    }
+
+    public void print(ArrayList<patient> patientTable){
+
+        for(int i =0; i<patientTable.size(); i++){
+            System.out.println(toString(patientTable.get(i)));
+        }
+    }
+}
+
+
